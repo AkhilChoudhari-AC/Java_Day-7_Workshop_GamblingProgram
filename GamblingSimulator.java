21608e82954bb717ed4444c9a35edda3de81a075
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-30 22:58:16 +0530

    REFACTOR: Win or Looses 50% of the Stakes

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -11,11 +11,19 @@ public class GamblingSimulator {
         Random rand = new Random();
         bet = rand.nextInt(2);
         System.out.println(bet);
-    if (bet == 1){
+   if (bet == 1){
         System.out.println("Game Win , Gambler makes 1$");
+        for(int i = 100; i<=150; i++){
+            System.out.println(i);
+        }
+       System.out.println("Gambler win 50 percent stakes that is 150\n Resign the game for the day ");
     }
     else {
-        System.out.println("Game Losses ,Gambler losses 1$");
-        }
+       System.out.println("Game Losses ,Gambler losses 1$");
+       for (int i = 100; i >= 50; i--) {
+           System.out.println(i);
+       }
+       System.out.println("Gambler Losses 50 percent stakes that is 50\n Resign the game for the day ");
+   }
     }
 }
