54a83449bfb5d857f073acbff8a6fe165ae04200
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-30 21:53:07 +0530

    ADDED: Game Win Or Losse

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,11 +1,21 @@
 package Day7WorkshopProgram;
 
+import java.util.Random;
+
 public class GamblingSimulator {
     public static void main(String[] args) {
        // Every day stakes = 100$
         int Stake = 100;
        // Every game bet = 1$
         int bet = 1;
-
+        Random rand = new Random();
+        bet = rand.nextInt(2);
+        System.out.println(bet);
+    if (bet == 1){
+        System.out.println("Game Win , Gambler makes 1$");
+    }
+    else {
+        System.out.println("Game Losses ,Gambler losses 1$");
+        }
     }
 }
