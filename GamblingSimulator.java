baeb622f705bd695185c1dc9826c614ce04a7af9
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-01 22:24:05 +0530

    ADDED: Total amount Win or Lose after 20 days.

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,22 +3,38 @@ package Day7WorkshopProgram;
 import java.util.Random;
 
 public class GamblingSimulator {
+    public static int TotalStake = 100;
+    public static int bit = 1;
+    public static int Won = 0;
+    public static int Loose = 0;
     public static void main(String[] args) {
-       // Every day stakes = 100$
-        int TotalStake = 100;
-       // Every game bet = 1$
+       double amount = 0;
+       int day;
+      for (day = 0; day < 20; day++) {
+          int stake = 100;
 
-      int stake = 100;
-        while (stake > (0.5 * TotalStake) && stake < (1.5 * TotalStake)) {
-            int bet = (int) (Math.floor(Math.random() * 10) % 2);
-            if (bet == 1) {
-                System.out.println("Won $1");
-                System.out.println("Initial Stake is $" + stake + ", won $1 and total is $"+(stake + bet));
-                stake++;
-            } else if (bet == 0) {
-                System.out.println("Loss $1");
-                System.out.println("Initial Stake is $" + stake + ", lost $1 and total is $"+(stake - bet));
-                stake--;
-            }
-        }
-}}
+          while (stake > (0.5 * TotalStake) && stake < (1.5 * TotalStake)) {
+              int bet = (int) (Math.floor(Math.random() * 10) % 2);
+              if (bet == 1) {
+                  Won++;
+                  stake++;
+              } else if (bet == 0) {
+                  Loose++;
+                  stake--;
+              }
+          }
+          if (stake > TotalStake) {
+              Won++;
+              amount = amount + 50;
+          } else if (stake < TotalStake) {
+              Loose++;
+              amount = amount - 50;
+          }
+      }
+      if (Won > Loose){
+          System.out.println("After Day " + day + ", the total amount won is $" + amount);
+      } else if (Loose > Won) {
+          System.out.println("After Day " + day + ", the total amount lose is $" + amount);
+      } else System.out.println("Neither Won nor Lose");
+    }
+}
