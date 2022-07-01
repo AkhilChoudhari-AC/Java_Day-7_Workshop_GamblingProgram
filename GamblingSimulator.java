6b6e76fcba76ebbc6efc0223465884d28571dc99
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-01 22:10:25 +0530

    REFACTOR: Win or Loose 50% of the Stakes.

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -5,25 +5,20 @@ import java.util.Random;
 public class GamblingSimulator {
     public static void main(String[] args) {
        // Every day stakes = 100$
-        int Stake = 100;
+        int TotalStake = 100;
        // Every game bet = 1$
-        int bet = 1;
-        Random rand = new Random();
-        bet = rand.nextInt(2);
-        System.out.println(bet);
-   if (bet == 1){
-        System.out.println("Game Win , Gambler makes 1$");
-        for(int i = 100; i<=150; i++){
-            System.out.println(i);
+
+      int stake = 100;
+        while (stake > (0.5 * TotalStake) && stake < (1.5 * TotalStake)) {
+            int bet = (int) (Math.floor(Math.random() * 10) % 2);
+            if (bet == 1) {
+                System.out.println("Won $1");
+                System.out.println("Initial Stake is $" + stake + ", won $1 and total is $"+(stake + bet));
+                stake++;
+            } else if (bet == 0) {
+                System.out.println("Loss $1");
+                System.out.println("Initial Stake is $" + stake + ", lost $1 and total is $"+(stake - bet));
+                stake--;
+            }
         }
-       System.out.println("Gambler win 50 percent stakes that is 150\n Resign the game for the day ");
-    }
-    else {
-       System.out.println("Game Losses ,Gambler losses 1$");
-       for (int i = 100; i >= 50; i--) {
-           System.out.println(i);
-       }
-       System.out.println("Gambler Losses 50 percent stakes that is 50\n Resign the game for the day ");
-   }
-    }
-}
+}}
