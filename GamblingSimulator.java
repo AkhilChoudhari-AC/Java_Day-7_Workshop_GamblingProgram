702870bb89cb49a113f391f34d36f2372acafde2
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-01 22:35:36 +0530

    REFACTOR: In each Month Days Win or Lose by how much.

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -5,36 +5,44 @@ import java.util.Random;
 public class GamblingSimulator {
     public static int TotalStake = 100;
     public static int bit = 1;
-    public static int Won = 0;
+    public static int Win = 0;
     public static int Loose = 0;
+    public static int Month = 0;
     public static void main(String[] args) {
-       double amount = 0;
-       int day;
-      for (day = 0; day < 20; day++) {
+        for (Month = 1; Month <= 12; Month++){
+            Win = 0;
+            Loose = 0;
+            int amount = 0;
+
+      for ( int day = 0; day < 30; day++) {
           int stake = 100;
 
           while (stake > (0.5 * TotalStake) && stake < (1.5 * TotalStake)) {
               int bet = (int) (Math.floor(Math.random() * 10) % 2);
               if (bet == 1) {
-                  Won++;
                   stake++;
               } else if (bet == 0) {
-                  Loose++;
                   stake--;
               }
           }
           if (stake > TotalStake) {
-              Won++;
+              Win++;
               amount = amount + 50;
           } else if (stake < TotalStake) {
               Loose++;
               amount = amount - 50;
           }
       }
-      if (Won > Loose){
-          System.out.println("After Day " + day + ", the total amount won is $" + amount);
-      } else if (Loose > Won) {
-          System.out.println("After Day " + day + ", the total amount lose is $" + amount);
-      } else System.out.println("Neither Won nor Lose");
+      if (Win > Loose){
+          System.out.println("Month " + Month + ", WON:" + Win + " times," + " LOSE:" + Loose + " times" + ", the total amount won is $" + amount);
+          float percent = (((Win - Loose) / 30) * 100);
+          System.out.println("Won by " + percent + "%");
+      } else if (Loose > Win) {
+          System.out.println("Month " + Month + ", WON:" + Win + " times," + " LOSE:" + Loose + " times" + ", the total amount lose is $" + amount);
+          float percent = (((Loose - Win) / 30) * 100);
+          System.out.println("Lose by " + percent + "%");
+      } else
+          System.out.println("Month " + Month + ", WON:" + Win + " times," + " LOSE:" + Loose + " times" + ", Neither Won nor Lose");
+        }
     }
 }
